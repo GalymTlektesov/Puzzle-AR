b0e964ff952a1b043ca3328904556c4641c12e38
Galym Tlektesov <galym2000gt@gmail.com>
2020-02-27 17:33:13 +0600

    Созданна рабочая программа

---
+++ Assets/Scripts/RotateInCamera.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+
+public class RotateInCamera : MonoBehaviour
+{
+
+    void Update()
+    {
+        transform.LookAt(Camera.main.transform);
+    }
+}
---
+++ Assets/Scripts/TrackableEventHandler.cs
@@ -0,0 +1,65 @@
+﻿using UnityEngine;
+using Vuforia;
+
+public class TrackableEventHandler : DefaultTrackableEventHandler
+{
+    public RotateInCamera Controller;
+
+    protected override void Start()
+    {
+        Controller = GetComponentInChildren<RotateInCamera>();
+        mTrackableBehaviour = GetComponent<TrackableBehaviour>();
+        if (mTrackableBehaviour)
+            mTrackableBehaviour.RegisterTrackableEventHandler(this);
+    }
+
+    protected override void OnDestroy()
+    {
+        if (mTrackableBehaviour)
+            mTrackableBehaviour.UnregisterTrackableEventHandler(this);
+    }
+
+    //Объект появляется в поле зрения камеры
+    protected override void OnTrackingFound()
+    {
+        if (mTrackableBehaviour)
+        {
+            Controller.gameObject.SetActive(true);
+
+            var rendererComponents = mTrackableBehaviour.GetComponentsInChildren<Renderer>(true);
+            var colliderComponents = mTrackableBehaviour.GetComponentsInChildren<Collider>(true);
+            var canvasComponents = mTrackableBehaviour.GetComponentsInChildren<Canvas>(true);
+
+            foreach (var component in rendererComponents)
+                component.enabled = true;
+
+            foreach (var component in colliderComponents)
+                component.enabled = true;
+
+            foreach (var component in canvasComponents)
+                component.enabled = true;
+        }
+    }
+
+    //Объект теряется с поле зрения камеры
+    protected override void OnTrackingLost()
+    {
+        if (mTrackableBehaviour)
+        {
+            Controller.gameObject.SetActive(false);
+
+            var rendererComponents = mTrackableBehaviour.GetComponentsInChildren<Renderer>(true);
+            var colliderComponents = mTrackableBehaviour.GetComponentsInChildren<Collider>(true);
+            var canvasComponents = mTrackableBehaviour.GetComponentsInChildren<Canvas>(true);
+
+            foreach (var component in rendererComponents)
+                component.enabled = false;
+
+            foreach (var component in colliderComponents)
+                component.enabled = false;
+
+            foreach (var component in canvasComponents)
+                component.enabled = false;
+        }
+    }
+}
